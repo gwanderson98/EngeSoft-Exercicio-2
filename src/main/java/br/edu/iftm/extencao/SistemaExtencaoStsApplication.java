3899539211c0cc2b17d01c93327da4515cf61e03
gwanderson98 <wanderson.guilherme@hotmail.com>
2019-09-09 14:10:19 -0300

    Versao inicial do projeto

---
+++ src/main/java/br/edu/iftm/extencao/SistemaExtencaoStsApplication.java
@@ -0,0 +1,13 @@
+package br.edu.iftm.extencao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaExtencaoStsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaExtencaoStsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/iftm/extencao/SistemaExtencaoStsApplicationTests.java
@@ -0,0 +1,16 @@
+package br.edu.iftm.extencao;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SistemaExtencaoStsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
